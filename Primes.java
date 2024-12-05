5ee2b60bf9833dd65b1a5ad21405e8a0904f1b7b
gideon-tzaig <gideon.tzaig@post.runi.ac.il>
2024-12-05 23:40:40 +0200

    HW4 - first 3 tests iteration 3

--- Primes.java
+++ Primes.java
@@ -1,7 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         // Create a boolean array to track prime numbers
-        int n = Integer.parseInt(args[0]);
+        int n = 7; // Integer.parseInt(args[0]);
         boolean[] isPrime = new boolean[n + 1];
 
         // Initialize all entries as true. A value in isPrime[i] will
@@ -31,7 +31,7 @@ public class Primes {
             }
         } 
 
-        int primesPrecentage = primesCounter/n;
+        int primesPrecentage = primesCounter*100/n;
         System.out.printf("There are %d primes between 2 and %d (%d%% are primes)", primesCounter, n, primesPrecentage);
     }
 }
